70d83d558fc41b5794f604fa9c7ab3949e66a537
Leanderp <leanderperez15@gmail.com>
2021-05-02 18:12:17 -0400

    Agregar archivos de proyecto.

---
+++ SignalRWeb/Hubs/PosicionHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRWeb.Hubs
+{
+    public class PosicionHub: Hub
+    {
+        public async Task EnviarPosicion(int lelf, int top)
+        {
+            await Clients.Others.SendAsync("RecibirPosicion", lelf, top);
+        }
+    }
+}
